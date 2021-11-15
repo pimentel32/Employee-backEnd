f0d691701a60d055ef86919fc39dfec32ee9338d
pimentel32 <85647425+pimentel32@users.noreply.github.com>
2021-11-15 13:09:04 -0500

    Update EmployeeResource.java

--- src/main/java/tech/getArrays/employeeManager/resource/EmployeeResource.java
+++ src/main/java/tech/getArrays/employeeManager/resource/EmployeeResource.java
@@ -9,7 +9,7 @@ import tech.getArrays.employeeManager.model.Employee;
 import tech.getArrays.employeeManager.service.EmployeeService;
 
 @RestController
-@CrossOrigin(origins = "http://pimentel32.github.io")
+@CrossOrigin(origins = "http://pimentel32.github.io",  allowedHeaders = "*")
 @RequestMapping("/employee")
 public class EmployeeResource {
 	private final EmployeeService employeeService;
